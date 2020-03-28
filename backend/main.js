6a6cdd748270b169bbd7bc1f8e34d6c25838d8b5
pbalbores <56827765+pbalbores@users.noreply.github.com>
2020-03-28 10:01:35 +0100

    Montamos servidor

--- backend/main.js
+++ backend/main.js
@@ -1,3 +1,25 @@
 //Inicamos proyecto.
 //Importamos Express
 const express = require('express');
+
+
+
+
+//Creamos servidor
+const server = express();
+
+//Llamadas
+server.get("/", (req, res) => {
+    res.send("Hola Mundo");
+});
+
+
+
+//Definimos puerto de servidor
+//const PORT = process.env.PORT;
+const PORT = 3000;
+
+//Ponemos servidor a escuchar
+server.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
